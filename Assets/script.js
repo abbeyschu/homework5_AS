705b8082b5a75a50d96c2bf6cd4e61ddae3ea3cb
Abbey Schueller <schuellerabbey@gmail.com>
2021-03-06 11:01:56 -0800

    updated readme and formatting

--- Assets/script.js
+++ Assets/script.js
@@ -110,85 +110,85 @@ const getText = () => {
 
 getText();
 
-// color code block based on current time (grey for past(default), blue for present, green for future)
+// color code block based on current time (grey for past(default), red for present, green for future)
 const colorCode = () => {
     var currentTime = moment().hour();
     console.log(moment().hour());
 
     if (currentTime >= 8 && currentTime < 9) {
-        textEight.style.background = "blue";
-        textNine.style.background = "green";
-        textTen.style.background = "green";
-        textEleven.style.background = "green";
-        textTwelve.style.background = "green";
-        textOne.style.background = "green";
-        textTwo.style.background = "green";
-        textThree.style.background = "green";
-        textFour.style.background = "green";
-        textFive.style.background = "green";
+        textEight.style.background = "#ff6961";
+        textNine.style.background = "#77dd77";
+        textTen.style.background = "#77dd77";
+        textEleven.style.background = "#77dd77";
+        textTwelve.style.background = "#77dd77";
+        textOne.style.background = "#77dd77";
+        textTwo.style.background = "#77dd77";
+        textThree.style.background = "#77dd77";
+        textFour.style.background = "#77dd77";
+        textFive.style.background = "#77dd77";
     }
     else if (currentTime>=9 && currentTime<10){
-        textNine.style.background = "blue"
-        textTen.style.background = "green";
-        textEleven.style.background = "green";
-        textTwelve.style.background = "green";
-        textOne.style.background = "green";
-        textTwo.style.background = "green";
-        textThree.style.background = "green";
-        textFour.style.background = "green";
-        textFive.style.background = "green";
+        textNine.style.background = "#ff6961"
+        textTen.style.background = "#77dd77";
+        textEleven.style.background = "#77dd77";
+        textTwelve.style.background = "#77dd77";
+        textOne.style.background = "#77dd77";
+        textTwo.style.background = "#77dd77";
+        textThree.style.background = "#77dd77";
+        textFour.style.background = "#77dd77";
+        textFive.style.background = "#77dd77";
     }
     else if (currentTime>=10 && currentTime<11){
-        textTen.style.background = "blue";
-        textEleven.style.background = "green";
-        textTwelve.style.background = "green";
-        textOne.style.background = "green";
-        textTwo.style.background = "green";
-        textThree.style.background = "green";
-        textFour.style.background = "green";
-        textFive.style.background = "green";
+        textTen.style.background = "#ff6961";
+        textEleven.style.background = "#77dd77";
+        textTwelve.style.background = "#77dd77";
+        textOne.style.background = "#77dd77";
+        textTwo.style.background = "#77dd77";
+        textThree.style.background = "#77dd77";
+        textFour.style.background = "#77dd77";
+        textFive.style.background = "#77dd77";
     }
     else if (currentTime>=11 && currentTime<12){
-        textEleven.style.background = "blue";
-        textTwelve.style.background = "green";
-        textOne.style.background = "green";
-        textTwo.style.background = "green";
-        textThree.style.background = "green";
-        textFour.style.background = "green";
-        textFive.style.background = "green";
+        textEleven.style.background = "#ff6961";
+        textTwelve.style.background = "#77dd77";
+        textOne.style.background = "#77dd77";
+        textTwo.style.background = "#77dd77";
+        textThree.style.background = "#77dd77";
+        textFour.style.background = "#77dd77";
+        textFive.style.background = "#77dd77";
     }
     else if (currentTime>=12 && currentTime<13){
-        textTwelve.style.background = "blue";
-        textOne.style.background = "green";
-        textTwo.style.background = "green";
-        textThree.style.background = "green";
-        textFour.style.background = "green";
-        textFive.style.background = "green";
+        textTwelve.style.background = "#ff6961";
+        textOne.style.background = "#77dd77";
+        textTwo.style.background = "#77dd77";
+        textThree.style.background = "#77dd77";
+        textFour.style.background = "#77dd77";
+        textFive.style.background = "#77dd77";
     }
     else if (currentTime>=13 && currentTime<14){
-        textOne.style.background = "blue";
-        textTwo.style.background = "green";
-        textThree.style.background = "green";
-        textFour.style.background = "green";
-        textFive.style.background = "green";
+        textOne.style.background = "#ff6961";
+        textTwo.style.background = "#77dd77";
+        textThree.style.background = "#77dd77";
+        textFour.style.background = "#77dd77";
+        textFive.style.background = "#77dd77";
     }
     else if (currentTime>=14 && currentTime<15){
-        textTwo.style.background = "blue";
-        textThree.style.background = "green";
-        textFour.style.background = "green";
-        textFive.style.background = "green";
+        textTwo.style.background = "#ff6961";
+        textThree.style.background = "#77dd77";
+        textFour.style.background = "#77dd77";
+        textFive.style.background = "#77dd77";
     }
     else if (currentTime>=15 && currentTime<16){
-        textThree.style.background = "blue";
-        textFour.style.background = "green";
-        textFive.style.background = "green";
+        textThree.style.background = "#ff6961";
+        textFour.style.background = "#77dd77";
+        textFive.style.background = "#77dd77";
     }
     else if (currentTime>=16 && currentTime<17){
-        textFour.style.background = "blue";
-        textFive.style.background = "green";
+        textFour.style.background = "#ff6961";
+        textFive.style.background = "#77dd77";
     }
     else if (currentTime>=17 && currentTime<18){
-        textFive.style.background = "blue";
+        textFive.style.background = "#ff6961";
     }
 };
 
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# homework5_AS
- Homework 05 Third-Party APIs: Work Day Scheduler
+# Work Day Scheduler Assignment
+
+## Description
+The goal of this project was to create a work day scheduler tool. It uses moment to show the date and time. Based on the current time, the hour blocks are color coded. Plans can be listed in each block and saved to local storage. 
+
+## Installation
+
+Visit this link to view the scheduler tool: https://abbeyschu.github.io/homework5_AS/
+
+## Screenshots
+
+![Image of Scheduler](Assets/Images/SchedulerScreenshot.png)
+
+
+## License 
+
+[MIT](https://github.com/abbeyschu/homework5_AS/raw/main/Assets/license.txt)
