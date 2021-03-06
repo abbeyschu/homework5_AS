f7c6cbf76cae0343f6c85c796ac3388c00aa8d5d
Abbey Schueller <schuellerabbey@gmail.com>
2021-03-05 21:55:37 -0800

    added color coding functionality

--- Assets/script.js
+++ Assets/script.js
@@ -24,9 +24,9 @@ const saveThree = document.getElementById('saveThree');
 const saveFour = document.getElementById('saveFour');
 const saveFive = document.getElementById('saveFive');
 
-
+// display the current date
 function displayDate() {
-    var rightNow = moment().format('dddd MMMM Do, YYYY');
+    var rightNow = moment().format('dddd MMMM Do, YYYY h:mm a');
     dateEl.text(rightNow);
 };
 
@@ -95,7 +95,7 @@ saveFive.addEventListener("click", saveFiveText);
 
 
 // get data from local storage
-const updateContent = () => {
+const getText = () => {
     textEight.innerHTML = localStorage.getItem("Eight");
     textNine.innerHTML = localStorage.getItem("Nine");
     textTen.innerHTML = localStorage.getItem("Ten");
@@ -108,4 +108,88 @@ const updateContent = () => {
     textFive.innerHTML = localStorage.getItem("Five");
 };
 
-updateContent();
+getText();
+
+// color code block based on current time (grey for past(default), blue for present, green for future)
+const colorCode = () => {
+    var currentTime = moment().hour();
+    console.log(moment().hour());
+
+    if (currentTime >= 8 && currentTime < 9) {
+        textEight.style.background = "blue";
+        textNine.style.background = "green";
+        textTen.style.background = "green";
+        textEleven.style.background = "green";
+        textTwelve.style.background = "green";
+        textOne.style.background = "green";
+        textTwo.style.background = "green";
+        textThree.style.background = "green";
+        textFour.style.background = "green";
+        textFive.style.background = "green";
+    }
+    else if (currentTime>=9 && currentTime<10){
+        textNine.style.background = "blue"
+        textTen.style.background = "green";
+        textEleven.style.background = "green";
+        textTwelve.style.background = "green";
+        textOne.style.background = "green";
+        textTwo.style.background = "green";
+        textThree.style.background = "green";
+        textFour.style.background = "green";
+        textFive.style.background = "green";
+    }
+    else if (currentTime>=10 && currentTime<11){
+        textTen.style.background = "blue";
+        textEleven.style.background = "green";
+        textTwelve.style.background = "green";
+        textOne.style.background = "green";
+        textTwo.style.background = "green";
+        textThree.style.background = "green";
+        textFour.style.background = "green";
+        textFive.style.background = "green";
+    }
+    else if (currentTime>=11 && currentTime<12){
+        textEleven.style.background = "blue";
+        textTwelve.style.background = "green";
+        textOne.style.background = "green";
+        textTwo.style.background = "green";
+        textThree.style.background = "green";
+        textFour.style.background = "green";
+        textFive.style.background = "green";
+    }
+    else if (currentTime>=12 && currentTime<13){
+        textTwelve.style.background = "blue";
+        textOne.style.background = "green";
+        textTwo.style.background = "green";
+        textThree.style.background = "green";
+        textFour.style.background = "green";
+        textFive.style.background = "green";
+    }
+    else if (currentTime>=13 && currentTime<14){
+        textOne.style.background = "blue";
+        textTwo.style.background = "green";
+        textThree.style.background = "green";
+        textFour.style.background = "green";
+        textFive.style.background = "green";
+    }
+    else if (currentTime>=14 && currentTime<15){
+        textTwo.style.background = "blue";
+        textThree.style.background = "green";
+        textFour.style.background = "green";
+        textFive.style.background = "green";
+    }
+    else if (currentTime>=15 && currentTime<16){
+        textThree.style.background = "blue";
+        textFour.style.background = "green";
+        textFive.style.background = "green";
+    }
+    else if (currentTime>=16 && currentTime<17){
+        textFour.style.background = "blue";
+        textFive.style.background = "green";
+    }
+    else if (currentTime>=17 && currentTime<18){
+        textFive.style.background = "blue";
+    }
+};
+
+colorCode();
